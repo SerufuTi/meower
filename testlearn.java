f1a26681c278536c8a9b1670f3e50e2309f8b0e6
SerufuTi <yinselfti@gmail.com>
2026-02-17 22:02:59 +0300

    test commit

---
+++ testlearn.java
@@ -0,0 +1,30 @@
+public class testlearn {  
+  
+    public static void main(String[] args) {  
+        System.out.print("Zig hail\\ \"\n\txc 1337 suka");  
+  
+        int pidor;  
+        pidor = 666;  
+            // Сколько пидоров на фейсите  
+                // Инт ну там дохуя чисел до 100к где-то, нахуй надо                // Вот лучше byte юзать, там от -128 до 127 можно использовать, ОПТИМИЗАЦИЯ (суки на valve)        System.out.println(pidor);  
+  
+        float age = 32.2f ;  
+            // Флоат как в контре, с точкой и ну в конце f надо добавлять - крутая штука  
+        System.out.println("Возраст пидораса: " + age + ".");  
+        short num = 10000;  
+            // Short - Больше чем byte, но и не такой большой как int.  
+        System.out.println(num);  
+  
+        char lox = 'q';  
+  
+        // Хуй знает зачем нужен char - для хранения одно символа? Ты че еблан совсем?  
+  
+        String pidoras = "boby alen";  
+        System.out.println(pidoras);  
+  
+        boolean testnaeblana = false;  
+  
+        //Ну или True - Это да или нет, выбор между разными кусками кода и действиями  
+    }  
+  
+}
